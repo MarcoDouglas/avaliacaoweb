7bcc5291e8ab4a5796b36fd846b29512de097f46
Marco Santos <marcomcd2@gmail.com>
2020-05-02 17:44:24 -0300

    Craicao dos Models

--- provas/models.py
+++ provas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente (models.Model):
+    nome= models.CharField('nome', max_length=100)
+    email=models.CharField('email', max_length=100)
+
+class Parceirias (models.Model):
+    nome= models.CharField('nome', max_length=100)
+    email=models.CharField('email', max_length=100)
+    proposta= models.CharField('proposta', max_length=100)
+
+class duvidas (models.Model):
+    nome= models.CharField('nome', max_length=100)
+    email=models.CharField('email', max_length=100)
+    duvidas= models.CharField('duvidas', max_length=100)
