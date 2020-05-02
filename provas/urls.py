16106e74821f65ed2817212af8f4d10e35077636
Marco Santos <marcomcd2@gmail.com>
2020-05-02 15:58:12 -0300

    Criacão de Rotas

---
+++ provas/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import index, contato, sobre, localizacao
+
+urlpatterns= [
+    path('',index),
+    path('contato',contato),
+    path('sobre',sobre),
+    path('localizacao',localizacao),
+]
--- provas/views.py
+++ provas/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
-def index(request):
-    return render(request,'contact.html')
+def contato(request):
+    return render(request, 'contato.html')
 
-def index(request):
-    return render(request,'sobre.html')
+def sobre(request):
+    return render(request, 'sobre.html')
 
-def index(request):
-    return render(request,'localizacao.html')
+def localizacao(request):
+    return render(request, 'localizacao.html')
