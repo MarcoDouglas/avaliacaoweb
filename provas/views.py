c69c0a1bfa0684c33ee520e3725665f80da6df76
Marco Santos <marcomcd2@gmail.com>
2020-05-02 16:18:25 -0300

    Criacao dos templates

--- provas/views.py
+++ provas/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+
 def contato(request):
     return render(request, 'contato.html')
 
+
 def sobre(request):
     return render(request, 'sobre.html')
+
 
 def localizacao(request):
     return render(request, 'localizacao.html')
