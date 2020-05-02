e5ada4ecaf42d31dc991379f0dd2a869ddff3709
Marco Santos <marcomcd2@gmail.com>
2020-05-02 15:41:52 -0300

    Criação de views

--- provas/views.py
+++ provas/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def index(request):
+    return render(request,'contact.html')
+
+def index(request):
+    return render(request,'sobre.html')
+
+def index(request):
+    return render(request,'localizacao.html')
