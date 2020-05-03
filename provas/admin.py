2aebfe8ad8cde269416aa9b21066dcfbc507cc8b
Marco Douglas <marcomcd2@gmail.com>
2020-05-03 19:37:40 -0300

    Criacao dos diretorios de JS,CSS,IMG;Criacao do css ;Edicao  Pagina Index

--- provas/admin.py
+++ provas/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Cliente, Parceirias, duvidas
+
 
 # Register your models here.
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+
+admin.site.register(Cliente,ClienteAdmin)
+
+class ParceiriasAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'proposta')
+
+admin.site.register(Parceirias,ParceiriasAdmin)
+
+class duvidasAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'duvidas')
+
+admin.site.register(duvidas,duvidasAdmin)
--- provas/models.py
+++ provas/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
-#Criado o Usuario e Senha do Django
+# Criado o Usuario e Senha do Django
 # TestCommit
 
-class Cliente (models.Model):
-    nome= models.CharField('nome', max_length=100)
-    email=models.CharField('email', max_length=100)
+class Cliente(models.Model):
+    nome = models.CharField('nome', max_length=100)
+    email = models.CharField('email', max_length=100)
 
-class Parceirias (models.Model):
-    nome= models.CharField('nome', max_length=100)
-    email=models.CharField('email', max_length=100)
-    proposta= models.CharField('proposta', max_length=100)
 
-class duvidas (models.Model):
-    nome= models.CharField('nome', max_length=100)
-    email=models.CharField('email', max_length=100)
-    duvidas= models.CharField('duvidas', max_length=100)
+class Parceirias(models.Model):
+    nome = models.CharField('nome', max_length=100)
+    email = models.CharField('email', max_length=100)
+    proposta = models.CharField('proposta', max_length=100)
+
+
+class duvidas(models.Model):
+    nome = models.CharField('nome', max_length=100)
+    email = models.CharField('email', max_length=100)
+    duvidas = models.CharField('duvidas', max_length=100)
