2957d751e486d77ab977360f719478506793a3f8
Marco Douglas <marcomcd2@gmail.com>
2020-05-03 19:47:39 -0300

    Adicionado menu em todas paginas

--- provas/urls.py
+++ provas/urls.py
@@ -3,6 +3,7 @@ from .views import index, contato, sobre, localizacao
 
 urlpatterns= [
     path('',index),
+    path('index',index),
     path('contato',contato),
     path('sobre',sobre),
     path('localizacao',localizacao),
